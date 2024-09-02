d490c9b2034435ed240009b67436df73ee346aef
tugcetorunn <tokerrtugcee@gmail.com>
2024-09-03 01:22:07 +0300

    Standart api response model created.

---
+++ TT.Core/ApiResponse/Response.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TT.Core.ApiResponse
+{
+    public class Response<T>
+    {
+        public int StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public T Data { get; set; }
+    }
+}
--- TT.Core/Authorization/TokenService.cs
+++ TT.Core/Authorization/TokenService.cs
@@ -1,17 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TT.Core.Authorization
 {
     public class TokenService
     {
+        // token mekanizmasını neden kullanırız ? 
+        // önce database imizde olan kullanıcıları kontrol mekanizmasından geçirip (email, password) kullanıcılarımızın içindense token üretiriz.
         private readonly IConfiguration configuration; // configuration dosyasından bilgi alacağımız için ekliyoruz.
 
         public TokenService(IConfiguration _configuration)
@@ -33,9 +31,13 @@ namespace TT.Core.Authorization
             var claims = new[] // her biri bir data tutar. new[] -> anonymous type.
             {
                 new Claim(JwtRegisteredClaimNames.Sub, userId), // token ın hangi kullanıcıya ait olduğunu belirtmek için kullanılır.
-                new Claim(JwtRegisteredClaimNames.Email, userEmail), // token ın hangi email e ait olduğunu belirtmek için kullanılır.
+                new Claim(JwtRegisteredClaimNames.Email, userEmail), // token ın hangi email e ait olduğunu belirtmek için kullanılır. sub, email vs bu paketin kendi claimleri. biz de kendi claim imizi tanım layabiliriz.
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // token ları benzersiz yapan mekanizma (jti), her token için farklı bir jti değeri oluşturur.
+                // new Claim("CompanyId", "110") // kendi claim lerimizi oluşturabiliriz.
             };
+            // payload kısmında claim olarak roller de tutulabilir. örneğin bir kişiye sadece read only erişim verilebilir. veya belli başlara endpointlere
+            // erişim sağlayabilir gibi...
+            // payload kısmı önemli çünkü, diyelim api mize biri istek attı. controller lardan kimin istek attığına tokendaki payload içerisinden tüm bilgilerine erişebiliriz.
 
             // Signature için gerekli
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)); // imza için kullanılacak secretkey i tekrar encode ediyor (tekrar şifreliyor yani (byte a dönüştürerek)) daha gizli hale getiriyor.
@@ -66,4 +68,7 @@ namespace TT.Core.Authorization
     // Signature - imza
         // header ve payload bilgilerini sırayla bir araya getirip, birleştirir ve bir imza oluşturur.
         // token ın güvenliğini sağlamayı ve değiştirilmeden kullanılmasını sağlar.
+
+    // jwt ye benzer bir başka erişim sınırlama aracı api key lerdir. fakat token lar kadar güvenli ve kapsamlı sayılmazlar.
+    // token lardan elde ettiğimiz bilgileri, api key lerden elde edemeyiz.
 }
